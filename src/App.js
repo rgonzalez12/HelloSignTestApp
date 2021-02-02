5de306ff5165d999a3d091ea5cdb64a4fe87d285
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2021-02-01 22:02:43 -0700

    updated app for submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloSign from 'hellosign-embedded'
+
+const client = new HelloSign({
+  clientId: 'f218efa0ab500c0eb26fc57071551479'
+});
+
+client.on('open', (data) => {
+  console.log(`The signature request was opened with ${data}`);
+});
+
+client.on('sign', (data) => {
+  console.log('The signature request was signed!');
+});
+
+client.on('cancel', () => {
+  console.log('The signature request was canceled');
+});
+
+client.on('close', () => {
+  console.log('The signature request window was closed');
+});
+
+
 
 function App() {
+  function signMe() {
+    client.open('https://app.hellosign.com/editor/embeddedSign?signature_id=08811593371a134c736b35a9519f030a&token=efbb0f81939f2107f86e6c4c45df9dbf', {skipDomainVerification: true});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signMe}>
+        Sign me
+      </button>
     </div>
   );
 }
